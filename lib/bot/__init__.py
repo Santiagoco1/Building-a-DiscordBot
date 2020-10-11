b1139e72372e964d6ba73610dc102a808a44b609
santiagoco1 <santiagoco2019@gmail.com>
2020-10-11 10:17:02 -0300

    Practicing with messages, embeds and attachments

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed, File
 from discord.ext.commands import Bot as BotBase
 
 PREFIX = "/"
@@ -16,7 +19,7 @@ class Bot(BotBase):
     def run(self, version):
         self.VERSION = version
 
-        with open("./lib/bot/token", "r", encoding="utf-8") as tf:
+        with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
             self.TOKEN = tf.read()
 
         print("Running Bot... ")
@@ -31,8 +34,27 @@ class Bot(BotBase):
     async def on_ready(self):
         if not self.ready:
             self.ready = True
+            self.guild = self.get_guild(753667457310261368)
             # self.guild = self.guild(753667457310261368)
             print("Bot Ready")
+
+            channel = self.get_channel(753667457310261371)
+            await channel.send("Now online!")
+
+            embed = Embed(tittle="Now Online!", description="GLaDOS is now online.", colour=0xFF0000, timestamp=datetime.utcnow())
+            fields = [("Name", "Value", True),
+                      ("Another field", "This field is next to the other one", True),
+                      ("A non-inline field", "This field will appear on it's own row", False)]
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+            embed.set_author(name="Aperture Science Laboratories", icon_url=self.guild.icon_url)
+            embed.set_footer(text="This is a footer!")
+            embed.set_thumbnail(url=self.guild.icon_url)
+            embed.set_image(url=self.guild.icon_url)
+            await channel.send(embed=embed)
+
+            await channel.send(file=File("./data/images/ApertureLaboratories.png"))
+
         else:
             print("Bot Reconnected")
 
